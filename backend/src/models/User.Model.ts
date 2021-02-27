70000bd26292f6a58f7433f702d520fa1b884fb6
niraj700 <nirajrawat667@gmail.com>
2021-02-27 20:52:16 +0530

    add user-model

---
+++ backend/src/models/User.Model.ts
@@ -0,0 +1,52 @@
+import { Schema, model } from "mongoose";
+import { IUser } from "../@types/User";
+
+import { compare, genSalt, hash } from "bcryptjs";
+
+const userSchema = new Schema<IUser>(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    contact :{
+      type : String,
+      required : true,
+    },
+    isNgo: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+    address: {
+      street: { type: String, required: true },
+      city: { type: String, required: true },
+      pincode: { type: String, required: true },
+      state: { type: String, required: true },
+    },
+  },
+  { timestamps: true }
+);
+
+userSchema.methods.matchPassword = async function (enteredPassword: string) {
+  return await compare(enteredPassword, this.password);
+};
+
+userSchema.pre("save", async function (this: IUser, next) {
+  if (!this.isModified("password")) {
+    next();
+  }
+  const salt = await genSalt(10);
+  this.password = await hash(this.password, salt);
+});
+
+export const User = model<IUser>("User", userSchema);
