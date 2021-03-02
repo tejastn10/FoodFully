d685be691c50ee486039ba99730cd044aab4dda2
tejastn10 <tejastn10@gmail.com>
2021-03-02 21:30:46 +0530

    🔗 ADD: Root Reducer

---
+++ frontend/app/src/store/reducers/reducer.ts
@@ -0,0 +1,10 @@
+import { combineReducers } from "@reduxjs/toolkit";
+
+const reducers = {
+};
+
+export const rootReducer = () => {
+  const reducer = combineReducers({ ...reducers });
+  return reducer;
+};
+
