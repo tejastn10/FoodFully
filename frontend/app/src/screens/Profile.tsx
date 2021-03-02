5b100ebcf3a377ba3dd997a258f592d12ee7fda6
tejastn10 <tejastn10@gmail.com>
2021-03-02 21:38:11 +0530

    📱 NEW: Profile Screen

---
+++ frontend/app/src/screens/Profile.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "../components/Themed";
+
+const Profile = () => {
+  return (
+    <View>
+      <Text>Profile</Text>
+    </View>
+  );
+};
+
+export default Profile;
