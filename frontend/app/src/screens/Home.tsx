d5d5cd1c0b85aa157d42158d10e896eeea018efb
tejastn10 <tejastn10@gmail.com>
2021-03-02 21:37:53 +0530

    📱 NEW: Home Screen

---
+++ frontend/app/src/screens/Home.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "../components/Themed";
+
+const Home = () => {
+  return (
+    <View>
+      <Text>Home</Text>
+    </View>
+  );
+};
+
+export default Home;
