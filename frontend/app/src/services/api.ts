296a03ea90ef2d302df92ffd1e47034af89b189a
tejastn10 <tejastn10@gmail.com>
2021-03-24 11:25:41 +0530

    🌐 NEW: Login and Register user APIs

--- frontend/app/src/services/api.ts
+++ frontend/app/src/services/api.ts
@@ -7,3 +7,16 @@ const API_ENDPOINT = isProd
   ? config.production.api_endpoint
   : config.development.api_endpoint;
 
+export const loginUser: any = ({ email, password }: any) => {
+  const URL = `${API_ENDPOINT}/api/users/login`;
+
+  const data: any = API.post(URL, { email, password });
+  return data;
+};
+
+export const registerUser: any = (user: any) => {
+  const URL = `${API_ENDPOINT}/api/users`;
+
+  return API.post(URL, user);
+};
+
