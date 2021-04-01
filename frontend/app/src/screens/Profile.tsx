bebaed994ccc8b1a35282123fdb2f7e072453827
tejastn10 <tejastn10@gmail.com>
2021-04-01 18:17:11 +0530

    📱 UPDATE: Profile Screen with Logout Button

--- frontend/app/src/screens/Profile.tsx
+++ frontend/app/src/screens/Profile.tsx
@@ -1,10 +1,22 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+import PrimaryBtn from "../components/PrimaryBtn";
 import { View, Text } from "../components/Themed";
+import { RouteStackParamList } from "../router/types";
+import { logoutUser } from "../store/actions/auth";
 
-const Profile = () => {
+const Profile: React.FC<RouteStackParamList<"Root">> = ({
+  navigation,
+}: RouteStackParamList<"Root">) => {
+  const dispatch = useDispatch();
+  const logoutHandler = () => {
+    dispatch(logoutUser());
+    navigation.replace("Login");
+  };
   return (
     <View>
       <Text>Profile</Text>
+      <PrimaryBtn title="Logout" onPress={logoutHandler} />
     </View>
   );
 };
