47dd0c0140a98d649ac928f09e7c81e88077a59b
tejastn10 <tejastn10@gmail.com>
2021-04-02 19:29:41 +0530

    Update app with Layout

--- frontend/web/src/App.tsx
+++ frontend/web/src/App.tsx
@@ -1,15 +1,26 @@
-import {BrowserRouter} from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
+import { Layout } from "antd";
+
+import { Navbar } from "./containers/auth/Nav";
+import { Footer } from "./components/Footer";
 import { Routes } from "./routes/routes";
 
-const App = () =>{
+const App = () => {
   return (
     <BrowserRouter>
-
-      <Routes />
-
-    
+      <Layout className="bg">
+        <Layout.Header className="hdr">
+          <Navbar />
+        </Layout.Header>
+        <Layout.Content className="cnt">
+          <Routes />
+        </Layout.Content>
+        <Layout.Footer className="ftr">
+          <Footer />
+        </Layout.Footer>
+      </Layout>
     </BrowserRouter>
   );
-}
+};
 
 export default App;
