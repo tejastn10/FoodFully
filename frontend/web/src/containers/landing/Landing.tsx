0de249de3bedc05cac23d0999bc6a5a765939aaa
tejastn10 <tejastn10@gmail.com>
2021-04-28 21:33:57 +0530

    UPDATE: Landing Container

--- frontend/web/src/containers/landing/Landing.tsx
+++ frontend/web/src/containers/landing/Landing.tsx
@@ -1,47 +1,89 @@
 import { Link } from "react-router-dom";
 
-const Landing = () => {
-  return (
-    <div>
-      <div id="showcase">
-        <div className="container">
-          <div className="showcase-content">
-            <h1>
-              <span className="text-primary">Feed Hungry</span> Feel Happy
-            </h1>
-            <p></p>
-            <Link className="btn" to="/login">
-              Login
-            </Link>
-            <p>OR</p>
-            <Link to="/register" className="btn">
-              Signup
-            </Link>
-          </div>
-        </div>
-      </div>
+import { Alert, Button, Card, Carousel } from "antd";
+import {
+  LoginOutlined,
+  UserAddOutlined,
+  ClockCircleOutlined,
+  TeamOutlined,
+} from "@ant-design/icons";
+import TextLoop from "react-text-loop";
+import { ApplicationState } from "../../store/store";
+import { useSelector } from "react-redux";
+import { AuthState } from "../../store/@types";
+import Ngo from "./Ngo";
+import Hotel from "./Hotel";
 
+const Landing = () => {
+  const authState = useSelector<ApplicationState, AuthState>(
+    (state) => state.auth
+  );
+
+  return authState.auth ? (
+    authState.auth?.isNgo ? (
+      <Ngo />
+    ) : (
+      <Hotel />
+    )
+  ) : (
+    <div className="container">
+      <Alert
+        banner
+        type="warning"
+        showIcon
+        message={
+          <TextLoop mask>
+            <div>We Stand against Hunger!!</div>
+            <div>Help us make this World Better!!</div>
+            <div>Donate Excess Food to the needy!!</div>
+          </TextLoop>
+        }
+      />
+      <section className="crd">
+        <Card
+          className="image-card"
+          cover={
+            <img
+              alt="hungry kids"
+              src="https://vistapointe.net/images/hunger-wallpaper-10.jpg"
+            />
+          }
+        >
+          <section className="crd-btn">
+            <Link to="/login">
+              <Button type="primary">
+                <LoginOutlined />
+                Log In
+              </Button>
+            </Link>
+            <Link to="/register">
+              <Button>
+                <UserAddOutlined />
+                Register
+              </Button>
+            </Link>
+          </section>
+        </Card>
+      </section>
       <section id="features">
-        <div className="box bg-light">
-          <img
-            src="./handshake-regular.svg"
-            className="features"
-            alt="handhake"
-          />
-          <h3>Organization</h3>
-          <p>
-            We have tied up with the trusted organizations to provide good and
-            healthy food !!!
-          </p>
-        </div>
-        <div className="box bg-primary">
-          <img src="./clock-regular.svg" className="features" alt="clock" />
-          <h3>Availability</h3>
-          <p>
-            Its our effort to provide food to the needy even at the worst time
-            and are to help 24/7.
-          </p>
-        </div>
+        <Carousel className="carousel" effect="fade" autoplay>
+          <div className="carousel-element">
+            <TeamOutlined style={{ fontSize: "2rem" }} />
+            <h3>Organization</h3>
+            <p>
+              We have tied up with the trusted organizations to provide good and
+              healthy food !!!
+            </p>
+          </div>
+          <div className="carousel-element">
+            <ClockCircleOutlined style={{ fontSize: "2rem" }} />
+            <h3>Availability</h3>
+            <p>
+              Its our effort to provide food to the needy even at the worst time
+              and are to help 24/7.
+            </p>
+          </div>
+        </Carousel>
       </section>
     </div>
   );
