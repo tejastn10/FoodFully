4ec9c78ce07f2ad7e61d38f1a1df5ed3e2610c00
tejastn10 <tejastn10@gmail.com>
2021-04-28 21:26:41 +0530

    Update Web Routes

--- frontend/web/src/routes/routes.tsx
+++ frontend/web/src/routes/routes.tsx
@@ -1,32 +1,17 @@
 import { Route, Switch } from "react-router-dom";
 
 import Login from "../containers/auth/Login";
-import Signup from "../containers/auth/Signup";
+import Register from "../containers/auth/Register";
 import About from "../components/About";
-import Ngo from "../containers/landing/Ngo";
 import Landing from "../containers/landing/Landing";
 import Contact from "../components/Contact";
-import Hotel from "../containers/landing/Hotel";
-import { AuthState } from "../store/@types";
-import { ApplicationState } from "../store/store";
-import { useSelector } from "react-redux";
 
 export const Routes = () => {
-  const authState = useSelector<ApplicationState, AuthState>(
-    (state) => state.auth
-  );
-
   return (
     <Switch>
-      <Route
-        exact
-        path="/"
-        component={
-          authState.auth ? (authState.auth?.isNgo ? Ngo : Hotel) : Landing
-        }
-      />
+      <Route exact path="/" component={Landing} />
       <Route exact path="/login" component={Login} />
-      <Route exact path="/register" component={Signup} />
+      <Route exact path="/register" component={Register} />
       <Route exact path="/about" component={About} />
       <Route exact path="/contact" component={Contact} />
     </Switch>
