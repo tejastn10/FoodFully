44867a6e0007a7bca6b3c090a4037729e15f2b07
tejastn10 <tejastn10@gmail.com>
2021-04-30 22:30:47 +0530

    ADD Profile Saga

--- frontend/web/src/store/sagas/saga.ts
+++ frontend/web/src/store/sagas/saga.ts
@@ -1,7 +1,8 @@
 import { all, fork } from "redux-saga/effects";
 import authSaga from "./auth";
 import nearbySaga from "./nearby";
+import profileSaga from "./profile";
 
 export const rootSaga = function* () {
-  yield all([fork(authSaga), fork(nearbySaga)]);
+  yield all([fork(authSaga), fork(nearbySaga), fork(profileSaga)]);
 };
