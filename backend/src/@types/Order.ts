1b63c7633a9b3c1c2b70ccde5a66afeeba4bb5fa
tejastn10 <tejastn10@gmail.com>
2021-05-01 21:37:16 +0530

    UPDATE Order Types and Model

--- backend/src/@types/Order.ts
+++ backend/src/@types/Order.ts
@@ -1,8 +1,26 @@
 import { Document } from "mongoose";
 
 export interface IOrder extends Document {
-  donation: String;
-  Ngo: String;
+  donation: {
+    _id: String;
+    quantity: String;
+    description: String;
+  };
+  Ngo: {
+    _id: String;
+    name: String;
+  };
+  hotel: {
+    _id: String;
+    name: String;
+    contact: String;
+    address: {
+      street: string;
+      city: string;
+      pincode: string;
+      state: string;
+    };
+  };
   delivered: Boolean;
   deliveredOn: Date;
 }
--- backend/src/models/Order.Model.ts
+++ backend/src/models/Order.Model.ts
@@ -1,25 +1,52 @@
 import { Schema, model } from "mongoose";
 import { IOrder } from "../@types/Order";
-import { Donation } from "./Donation.Model";
-import { User } from "./User.Model";
+
+const donationSchema: Schema = new Schema({
+  _id: {
+    type: Schema.Types.ObjectId,
+    required: true,
+    ref: "Donation",
+  },
+  quantity: { type: String, required: true },
+  description: { type: String, required: true },
+});
+
+const ngoSchema: Schema = new Schema({
+  _id: {
+    type: Schema.Types.ObjectId,
+    required: true,
+    ref: "User",
+  },
+  name: { type: String, required: true },
+});
+
+const hotelSchema: Schema = new Schema({
+  _id: {
+    type: Schema.Types.ObjectId,
+    required: true,
+    ref: "User",
+  },
+  name: { type: String, required: true },
+  contact: { type: String, required: true },
+  address: {
+    street: { type: String, required: true },
+    city: { type: String, required: true },
+    pincode: { type: String, required: true },
+    state: { type: String, required: true },
+  },
+});
 
 const orderSchema = new Schema<IOrder>({
-  donation: {
-    type: Schema.Types.ObjectId,
-    required: true,
-    ref: Donation,
-  },
-  Ngo: {
-    type: Schema.Types.ObjectId,
-    required: true,
-    ref: User,
-  },
+  donation: donationSchema,
+  hotel: hotelSchema,
+  Ngo: ngoSchema,
   delivered: {
     type: Boolean,
     default: false,
   },
   deliveredOn: {
     type: Date,
+    default: "",
   },
 });
 export const Order = model<IOrder>("Order", orderSchema);
