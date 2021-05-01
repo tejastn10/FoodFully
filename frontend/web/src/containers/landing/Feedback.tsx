e978b8e5088664b5fb42b85efc52c2e2aaa239ce
tejastn10 <tejastn10@gmail.com>
2021-05-01 21:36:27 +0530

    Update Feedback Modal

--- frontend/web/src/containers/landing/Feedback.tsx
+++ frontend/web/src/containers/landing/Feedback.tsx
@@ -1,15 +1,23 @@
 import { Modal, Result } from "antd";
 import { Dispatch, SetStateAction } from "react";
 import { Donation } from "../../store/@types/donation";
+import { Order } from "../../store/@types/order";
 
 type Props = {
   visible: boolean;
   setVisible: Dispatch<SetStateAction<boolean>>;
   donation?: Donation | null;
+  order?: Order | null;
   isNgo?: boolean;
 };
 
-const Feedback = ({ visible, setVisible, donation, isNgo = false }: Props) => {
+const Feedback = ({
+  visible,
+  setVisible,
+  donation,
+  order,
+  isNgo = false,
+}: Props) => {
   return (
     <>
       {isNgo ? (
@@ -21,14 +29,26 @@ const Feedback = ({ visible, setVisible, donation, isNgo = false }: Props) => {
           onCancel={() => setVisible(false)}
         >
           <Result
-            status="error"
-            title="Some Error Occured! Please try again!"
-
-            // subTitle={[
-            //   <p>Donation ID: {donation?._id}</p>,
-            //   <p>Donation Time: {donation?.donatedOn.substring(0, 10)}</p>,
-            //   <p>Please Wait the NGO's will get in touch with you</p>,
-            // ]}
+            status={order?._id ? "success" : "error"}
+            title={
+              order?._id
+                ? "You have accepted the Donation! Please Contact the Hotel"
+                : "Some Error Occured! Please try again!"
+            }
+            subTitle={
+              order?._id
+                ? [
+                    <p>Donation ID: {order?.donation._id}</p>,
+                    <p>Hotel Name: {order?.hotel.name}</p>,
+                    <p>Hotel Contact: {order?.hotel.contact}</p>,
+                    <p>
+                      Hotel Address: {order?.hotel.address.street},{" "}
+                      {order?.hotel.address.city}, {order?.hotel.address.state}{" "}
+                      - {order?.hotel.address.pincode}
+                    </p>,
+                  ]
+                : null
+            }
           />
         </Modal>
       ) : (
@@ -36,7 +56,6 @@ const Feedback = ({ visible, setVisible, donation, isNgo = false }: Props) => {
           centered
           visible={visible}
           footer={null}
-          okText="Add"
           onCancel={() => setVisible(false)}
         >
           <Result
@@ -46,11 +65,17 @@ const Feedback = ({ visible, setVisible, donation, isNgo = false }: Props) => {
                 ? "Successfully Placed your Donation!"
                 : "Some Error Occured! Please try again!"
             }
-            subTitle={[
-              <p>Donation ID: {donation?._id}</p>,
-              <p>Donation Time: {donation?.donatedOn.substring(0, 10)}</p>,
-              <p>Please Wait the NGO's will get in touch with you</p>,
-            ]}
+            subTitle={
+              donation?._id
+                ? [
+                    <p>Donation ID: {donation?._id}</p>,
+                    <p>
+                      Donation Time: {donation?.donatedOn.substring(0, 10)}
+                    </p>,
+                    <p>Please Wait the NGO's will get in touch with you</p>,
+                  ]
+                : null
+            }
           />
         </Modal>
       )}
