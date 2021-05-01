2e7b6ec92b71078c60446559460d5436319a0509
tejastn10 <tejastn10@gmail.com>
2021-05-01 14:25:05 +0530

    UPDATE: Feedback Modal

--- frontend/web/src/containers/landing/Feedback.tsx
+++ frontend/web/src/containers/landing/Feedback.tsx
@@ -1,12 +1,14 @@
 import { Modal, Result } from "antd";
 import { Dispatch, SetStateAction } from "react";
+import { Donation } from "../../store/@types/donation";
 
 type Props = {
   visible: boolean;
   setVisible: Dispatch<SetStateAction<boolean>>;
+  donation?: Donation | null;
 };
 
-const Feedback = ({ visible, setVisible }: Props) => {
+const Feedback = ({ visible, setVisible, donation }: Props) => {
   return (
     <Modal
       centered
@@ -16,10 +18,15 @@ const Feedback = ({ visible, setVisible }: Props) => {
       onCancel={() => setVisible(false)}
     >
       <Result
-        status="success"
-        title="Successfully Placed your Donation!"
+        status={donation?._id ? "success" : "error"}
+        title={
+          donation?._id
+            ? "Successfully Placed your Donation!"
+            : "Some Error Occured! Please try again!"
+        }
         subTitle={[
-          <p>Donation ID:</p>,
+          <p>Donation ID: {donation?._id}</p>,
+          <p>Donation Time: {donation?.donatedOn}</p>,
           <p>Please Wait the NGO's will get in touch with you</p>,
         ]}
       />
