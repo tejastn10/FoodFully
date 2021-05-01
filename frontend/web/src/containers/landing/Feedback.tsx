45ad569e95dd7b8379b967eda753fb72f3605859
tejastn10 <tejastn10@gmail.com>
2021-05-01 16:27:13 +0530

    UPDATE: Feedback Modal

--- frontend/web/src/containers/landing/Feedback.tsx
+++ frontend/web/src/containers/landing/Feedback.tsx
@@ -6,31 +6,55 @@ type Props = {
   visible: boolean;
   setVisible: Dispatch<SetStateAction<boolean>>;
   donation?: Donation | null;
+  isNgo?: boolean;
 };
 
-const Feedback = ({ visible, setVisible, donation }: Props) => {
+const Feedback = ({ visible, setVisible, donation, isNgo = false }: Props) => {
   return (
-    <Modal
-      centered
-      visible={visible}
-      footer={null}
-      okText="Add"
-      onCancel={() => setVisible(false)}
-    >
-      <Result
-        status={donation?._id ? "success" : "error"}
-        title={
-          donation?._id
-            ? "Successfully Placed your Donation!"
-            : "Some Error Occured! Please try again!"
-        }
-        subTitle={[
-          <p>Donation ID: {donation?._id}</p>,
-          <p>Donation Time: {donation?.donatedOn.substring(0, 10)}</p>,
-          <p>Please Wait the NGO's will get in touch with you</p>,
-        ]}
-      />
-    </Modal>
+    <>
+      {isNgo ? (
+        <Modal
+          centered
+          visible={visible}
+          footer={null}
+          okText="Add"
+          onCancel={() => setVisible(false)}
+        >
+          <Result
+            status="error"
+            title="Some Error Occured! Please try again!"
+
+            // subTitle={[
+            //   <p>Donation ID: {donation?._id}</p>,
+            //   <p>Donation Time: {donation?.donatedOn.substring(0, 10)}</p>,
+            //   <p>Please Wait the NGO's will get in touch with you</p>,
+            // ]}
+          />
+        </Modal>
+      ) : (
+        <Modal
+          centered
+          visible={visible}
+          footer={null}
+          okText="Add"
+          onCancel={() => setVisible(false)}
+        >
+          <Result
+            status={donation?._id ? "success" : "error"}
+            title={
+              donation?._id
+                ? "Successfully Placed your Donation!"
+                : "Some Error Occured! Please try again!"
+            }
+            subTitle={[
+              <p>Donation ID: {donation?._id}</p>,
+              <p>Donation Time: {donation?.donatedOn.substring(0, 10)}</p>,
+              <p>Please Wait the NGO's will get in touch with you</p>,
+            ]}
+          />
+        </Modal>
+      )}
+    </>
   );
 };
 
