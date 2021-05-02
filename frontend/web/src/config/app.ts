55d1cc95aea8dce1f28f2ba8b3852625df60f539
tejastn10 <tejastn10@gmail.com>
2021-05-02 17:27:13 +0530

    ADD Production Endpoint

--- frontend/web/src/config/app.ts
+++ frontend/web/src/config/app.ts
@@ -1,21 +1,23 @@
 type Config = {
-    isProd: boolean;
-    production: {
-      api_endpoint: string;
-    };
-    development: {
-      api_endpoint: string;
-    };
+  isProd: boolean;
+  production: {
+    api_endpoint: string;
   };
-  
-  const config: Config = {
-    isProd: process.env.NODE_ENV === "production",
-    production: {
-      api_endpoint: process.env.REACT_APP_PRODUCTION_API_ENDPOINT || "",
-    },
-    development: {
-      api_endpoint: process.env.REACT_APP_DEVELOPMENT_API_ENDPOINT || "",
-    },
+  development: {
+    api_endpoint: string;
   };
-  
-  export default config;
+};
+
+const config: Config = {
+  isProd: process.env.NODE_ENV === "production",
+  production: {
+    api_endpoint:
+      process.env.REACT_APP_PRODUCTION_API_ENDPOINT ||
+      "https://foodfully.herokuapp.com/",
+  },
+  development: {
+    api_endpoint: process.env.REACT_APP_DEVELOPMENT_API_ENDPOINT || "",
+  },
+};
+
+export default config;
